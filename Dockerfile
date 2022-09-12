a0a44bda91820ebdc7c1b0f49a1a629034c3dc32
dirty-no <sonicsupersayen@gmail.com>
2022-09-12 15:05:39 +0000

    fix(upload): Fixes data race and path traversal

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM nginx:latest
 
-RUN apt update && apt install -yqq fcgiwrap jq curl file xxd
+RUN apt update && apt install -yqq fcgiwrap jq curl file xxd uuid
 
 CMD /etc/init.d/fcgiwrap start && chmod -R 777 /run/fcgiwrap.socket /cgi-bin /templates /uploads && bash docker-entrypoint.sh  nginx -g "daemon off;"
--- src/cgi/upload.sh
+++ src/cgi/upload.sh
@@ -3,12 +3,11 @@
 echo "Content-type: image/png"
 echo ""
 
-FILEPATH=/uploads/test_img
+FILEPATH=/tmp/$(uuid)_$(date +%s)
 
 cat > $FILEPATH
 
-FILENAME="/uploads/$(awk <$FILEPATH -F '"' 'NR==2{print $4}')"
+FILENAME="/uploads/$(basename $(awk <$FILEPATH -F '"' 'NR==2{print $4}'))"
 awk <"$FILEPATH" 'NR>5 {print last} {last=$0}' > "$FILENAME"
-
 
 cat "$FILENAME"
